68e0f937ac1da4734d3138131adebf6bc899dbfb
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2017-11-24 00:18:07 +0200

    Minor Tweak

--- Scoring System/MainForm.cs
+++ Scoring System/MainForm.cs
@@ -16,6 +16,7 @@ namespace Scoring_System
         {
             if (DynamicTraineeText.Text != "")
             {
+                ActionPanel.AutoScrollPosition = new System.Drawing.Point(0, 0);
                 Trainees.Add(new Trainee
                 {
                     Name = DynamicTraineeText.Text
